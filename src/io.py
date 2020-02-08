5ee8e91b9850e5baa8036547ae5fb79598b363dc
Ben Webb <ben@salilab.org>
2020-02-07 21:24:19 -0800

    Add colors to beads if present in the RMF file

--- src/io.py
+++ src/io.py
@@ -7,35 +7,47 @@ def open_rmf(session, path):
     Returns the 2-tuple return value appropriate for the
     ``chimerax.core.toolshed.BundleAPI.open_file`` method.
     """
-    from . import RMF
-    from chimerax.atomic import Structure
-
-    structures = []
-
-    r = RMF.open_rmf_file_read_only(path)
-    # todo, actually read the file
-    nframes = r.get_number_of_frames()
-    r.set_current_frame(RMF.FrameID(0))
-
-    pf = RMF.ParticleConstFactory(r)
-    s = Structure(session)
-    res = s.new_residue('ALA', 'A', 1)
-    _handle_node(r.get_root_node(), pf, s, res)
-    structures.append(s)
+    rl = _RMFLoader()
+    r, structures = rl.load(path, session)
 
     status = ("Opened RMF file produced with %s, with %d frames"
-              % (r.get_producer(), nframes))
+              % (r.get_producer(), r.get_number_of_frames()))
     return structures, status
 
 
-def _handle_node(node, pf, s, res):
-    if pf.get_is(node):
-        p = pf.get(node)
-        atom = s.new_atom('C', 'C')
-        atom.coord = p.get_coordinates()
-        atom.mass = p.get_mass()
-        atom.radius = p.get_radius()
-        atom.draw_mode = atom.SPHERE_STYLE
-        res.add_atom(atom)
-    for child in node.get_children():
-        _handle_node(child, pf, s, res)
+class _RMFLoader(object):
+    def __init__(self):
+        pass
+
+    def load(self, path, session):
+        from . import RMF
+        from chimerax.atomic import Structure
+
+        structures = []
+        r = RMF.open_rmf_file_read_only(path)
+        self.particlef = RMF.ParticleConstFactory(r)
+        self.coloredf = RMF.ColoredConstFactory(r)
+
+        # todo, actually read the file
+        r.set_current_frame(RMF.FrameID(0))
+
+        s = Structure(session)
+        res = s.new_residue('ALA', 'A', 1)
+        self._handle_node(r.get_root_node(), s, res)
+        return r, [s]
+
+    def _handle_node(self, node, s, res):
+        if self.particlef.get_is(node):
+            p = self.particlef.get(node)
+            atom = s.new_atom('C', 'C')
+            atom.coord = p.get_coordinates()
+            atom.mass = p.get_mass()
+            atom.radius = p.get_radius()
+            atom.draw_mode = atom.SPHERE_STYLE
+            if self.coloredf.get_is(node):
+                c = self.coloredf.get(node)
+                # RMF colors are 0-1 and has no alpha; ChimeraX uses 0-255
+                atom.color = [x * 255. for x in c.get_rgb_color()] + [255]
+            res.add_atom(atom)
+        for child in node.get_children():
+            self._handle_node(child, s, res)
