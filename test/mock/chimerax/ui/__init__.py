ee764e8184279deb814dc8ab91e5a06b9230047c
Ben Webb <ben@salilab.org>
2020-02-26 19:27:30 -0800

    Use singleton QApplication
    Creating a new application object for each
    session is probably inefficient and it seems
    to make PyQt5 crash on an occasional basis.

--- test/mock/chimerax/ui/__init__.py
+++ test/mock/chimerax/ui/__init__.py
@@ -1,12 +1,13 @@
 from PyQt5.QtWidgets import QApplication, QWidget
 
+app = QApplication([])
+
 class _MockQWidget:
     def setLayout(self, layout):
         pass
 
 class MainToolWindow:
     def __init__(self, tool_instance, **kw):
-        self.app = QApplication([])
         self.ui_area = QWidget()
 
     def manage(self, placement, fixed_size=False, allowed_areas=None):
